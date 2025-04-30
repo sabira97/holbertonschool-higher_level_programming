f591920d84a7d0d51dc77fe247d1274933dbdfb7
sabira97 <sbirabbasova1@gmail.com>
2025-04-30 13:16:43 +0000

    task

--- 8-uppercase.py
+++ 8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     result = ""
     for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            result+= chr(ord(char) - 32)
+        if 'a' <= ord(char) <= ord('z'):
+            result += chr(ord(char) - 32)
         else:
             result += char
-    print("{}".format(result))
+    print("{}".format(result) )

550b1d5814e7c848b2240708d482a0c7d8700c31
sabira97 <sbirabbasova1@gmail.com>
2025-05-05 14:07:27 +0000

    task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list(my_list, x):
+def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
             count += 1
-    except IndexError:
-        pass  # Listdə element qurtararsa, heç nə etmə
-    print()  # Yuxarıda çap olunan elementlərin ardınca bir yeni sətir əlavə et
+        except IndexError:
+            break
+    print()  # Print a new line after the elements
     return count 

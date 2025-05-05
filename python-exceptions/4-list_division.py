64a8d28d097e78ad8d32e9a9410700ac29ecc034
sabira97 <sbirabbasova1@gmail.com>
2025-05-05 13:01:45 +0000

    task4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+
+    for i in range(list_length):
+        try:
+            
+            num1 = my_list_1[i]
+            num2 = my_list_2[i]
+
+
+            if isinstance(num1, (int, float)) and isinstance(num2, (int, float)):
+                
+                result.append(num1 / num2)
+            else:
+                raise TypeError
+        except IndexError:
+            
+            print("out of range")
+            result.append(0)
+        except ZeroDivisionError:
+            
+            print("division by 0")
+            result.append(0)
+        except TypeError:
+            
+            print("wrong type")
+            result.append(0)
+        finally:
+            
+            pass
+
+    return result

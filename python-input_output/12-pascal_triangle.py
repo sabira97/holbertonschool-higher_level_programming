dcaff401016981e1d821d112802d31b85dc2df3e
sabira97 <sbirabbasova1@gmail.com>
2025-06-10 12:59:31 +0000

    task12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-"""Function to generate Pascal's triangle."""
+"""Module that defines pascal_triangle function."""
 
 
 def pascal_triangle(n):
-    """Returns a list of lists representing Pascal’s triangle of n."""
+    """
+    Returns a list of lists of integers representing Pascal's triangle of n.
+
+    Args:
+        n (int): The number of rows of the triangle.
+
+    Returns:
+        list: A list of lists representing the triangle.
+    """
     if n <= 0:
         return []
 
-    triangle = [[1]]  # First row
+    triangle = [[1]]
 
     for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
+        row = [1]  # Hər sətrin ilk elementi 1-dir
+        last_row = triangle[i - 1]
 
+        # Hər elementi üst sətrin iki qonşu elementinin cəmi olaraq tapırıq
         for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
+            row.append(last_row[j - 1] + last_row[j])
 
-        new_row.append(1)
-        triangle.append(new_row)
+        row.append(1)  # Hər sətrin son elementi də 1-dir
+        triangle.append(row)
 
     return triangle

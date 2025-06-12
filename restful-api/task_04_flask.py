33bd54656cdae6877c0615dfb4a888141e42db6e
sabira97 <sbirabbasova1@gmail.com>
2025-06-12 19:37:41 +0400

    task

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"},
+}
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route("/data")
+def get_usernames():
+    return jsonify(list(users.keys()))
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<username>")
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    username = data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    users[username] = {
+        "username": username,
+        "name": data.get("name", ""),
+        "age": data.get("age", 0),
+        "city": data.get("city", "")
+    }
+
+    return jsonify({
+        "message": "User added",
+        "user": users[username]
+    }), 201
+
+if __name__ == "__main__":
+    app.run()
